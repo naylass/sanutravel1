1a786bf9e6cf8043ea91a27517d9993ddaacb3a7
lumesamasaikosugiru <lumetrixsynf@gmail.com>
2026-04-16 19:15:05 +0700

    add : Spatie role permission || changes : seeder & user model

---
+++ database/seeders/RolePermissionSeeder.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class RolePermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        app(\Spatie\Permission\PermissionRegistrar::class)->forgetCachedPermissions();
+
+        //kode ini buat nambah role akun
+        $roles = [
+            'superadmin',
+            'admin',
+            'driver',
+            'customer',
+        ];
+
+        foreach ($roles as $role) {
+            Role::firstOrCreate(['name' => $role]);
+        }
+        //kode ini buat nambah permission
+        $permissions = [
+            'manage.users',
+            'manage.drivers',
+            'manage.services',
+            'manage.vehicles',
+
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::firstOrCreate(['name' => $permission]);
+        }
+
+        //kode ini buat ngasih permission ke akun
+        Role::findByName('superadmin')
+            ->givePermissionTo(Permission::all());
+
+        Role::findByName('admin')
+            ->givePermissionTo([
+                'manage.drivers',
+                'manage.services',
+                'manage.vehicles',
+            ]);
+
+        Role::findByName('driver')
+            ->givePermissionTo([
+                '',
+            ]);
+
+        Role::findByName('customer')
+            ->givePermissionTo([
+                ''
+            ]);
+    }
+}
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -18,29 +18,40 @@ class UserSeeder extends Seeder
 
         DB::table('users')->insert([
             [
+                'name' => 'Superadmin',
+                'email' => 'superadmin@gmail.com',
+                'password' => Hash::make('admin123'),
+                'role' => 'superadmin',
+            ],
+            [
                 'name' => 'Admin',
                 'email' => 'naysaj@gmail.com',
                 'password' => Hash::make('admin123'),
+                'role' => 'admin',
             ],
             [
                 'name' => 'Syahnuri',
                 'email' => 'Syahnuri@gmail.com',
                 'password' => Hash::make('admin123'),
+                'role' => 'driver',
             ],
             [
                 'name' => 'Atika',
                 'email' => 'atika@gmail.com',
                 'password' => Hash::make('atika123'),
+                'role' => 'customer',
             ],
             [
                 'name' => 'Anjeng',
                 'email' => 'anjeng@gmail.com',
                 'password' => Hash::make('anjeng123'),
+                'role' => 'customer',
             ],
             [
                 'name' => 'Nazwa',
                 'email' => 'nazwa@gmail.com',
                 'password' => Hash::make('nazwa123'),
+                'role' => 'customer',
             ],
         ]);
     }
